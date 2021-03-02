0955b78624bf3941e6ba9e71ddfc703e940b15bf
Mayorov Yury <mayst1230@gmail.com>
2021-03-02 15:26:19 +0400

    add classes

---
+++ ComputerEquipmentStore/ComputerStoreEquipmentBusinessLogic/Seller/ViewModels/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerEquipmentStoreBusinessLogic.Seller.ViewModels
+{
+    class OrderViewModel
+    {
+    }
+}
